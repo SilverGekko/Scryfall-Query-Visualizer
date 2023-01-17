d8ce3f8c2ef6a61a970adcba35849733c4bad291
Nicholas Pugliese <nicholas.d.pugliese@gmail.com>
2023-01-17 00:28:36 -0800

    Update main.py
    +move reprint selector to end
    +change size of plot window
    +rename variables to better things
    +name plot axes and title

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import time
 import sys
 
 if __name__ == "__main__":
-  unique = "game:paper legal:commander -set:sld"
+  today_year = datetime.date.today().year
+  main_selectors = "game:paper legal:commander -set:sld"
   selectors = {
     "legends" : "is:commander",
     "expensive" : "usd>=10",
@@ -16,15 +17,15 @@ if __name__ == "__main__":
   }
   api_url = "https://api.scryfall.com/cards/search?&q=year:{0} {1} {2} {3}"
   data = {}
-  extra = ""
+  extra_selectors = ""
   unique_or_reprint = "is:firstprinting"
   if len(sys.argv) > 1:
-    extra = selectors[sys.argv[1]]
-  if len (sys.argv) > 2:
+    extra_selectors = selectors[sys.argv[1]]
+  if sys.argv[-1:]:
     unique_or_reprint = "is:reprint"
   print("Collecting Data")
-  for year in range(1993, datetime.date.today().year):
-    formatted_query = api_url.format(year, unique, unique_or_reprint, extra)
+  for year in range(1993, today_year):
+    formatted_query = api_url.format(year, main_selectors, extra_selectors, unique_or_reprint)
     print(formatted_query)
     response = requests.get(formatted_query)
     json_res = response.json()
@@ -37,5 +38,11 @@ if __name__ == "__main__":
   print("\nData Collection Finished")
   names = list(data.keys())
   values = list(data.values())
+  plt.xlabel('Amount')
+  plt.ylabel('Year')
+  plt.title("/cards/search?&q=year:{0} {1} {2} {3}".format("1993-" + str(today_year), main_selectors, extra_selectors, unique_or_reprint))
+  f = plt.figure(num=1)
+  f.set_figwidth(20)
+  f.set_figheight(8)
   plt.bar(range(len(data)), values, tick_label=names)
   plt.show()
