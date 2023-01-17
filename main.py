63faa8240a166c021d0e5cce6925ef39b59731f0
Nicholas Pugliese <nicholas.d.pugliese@gmail.com>
2023-01-16 23:34:51 -0800

    Update main.py
    + support for different queries, selected from command line args
    + query for unique commanders

--- main.py
+++ main.py
@@ -4,16 +4,27 @@ import numpy as np
 import random
 import datetime
 import time
+import sys
 
 if __name__ == "__main__":
-  selectors = "game:paper legal:commander -set:sld is:firstprinting"
-  api_url = "https://api.scryfall.com/cards/search?&q=year:{0} {1}"
+  unique = "game:paper legal:commander -set:sld is:firstprinting"
+  selectors = {
+    "legends" : "is:commander"
+  }
+  api_url = "https://api.scryfall.com/cards/search?&q=year:{0} {1} {2}"
   data = {}
+  extra = ""
+  if len(sys.argv) > 1:
+    extra = selectors[sys.argv[1]]
   print("Collecting Data")
   for year in range(1993, datetime.date.today().year):
-    response = requests.get(api_url.format(year, selectors))
-    response.json()
-    data[year] = response.json()["total_cards"]
+    print(api_url.format(year, unique, extra))
+    response = requests.get(api_url.format(year, unique, extra))
+    json_res = response.json()
+    if "total_cards" in json_res:
+      data[year] = json_res["total_cards"]
+    else:
+      data[year] = 0
     time.sleep(random.uniform(0.5, 1.5)) #don't rate limit me bro
   
   print("\nData Collection Finished")
